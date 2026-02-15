c2380a6602d5869218a03b5f5733b50855cd052d
花火 <zql3322r@outlook.com>
2026-02-16 00:16:31 +0800

    解决部署问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sparkle636.github.io",
-	base: "/AstroBlog",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
