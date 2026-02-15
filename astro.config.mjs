34b91d440f963aaf5507f64b660fbc7a4ba3cbb7
花火 <zql3322r@outlook.com>
2026-02-15 23:11:58 +0800

    博客更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,11 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< HEAD
 	site: "https://sparkle636.github.io",
+=======
+	site: "https://sparkle636.github.io/",
+>>>>>>> 77ca12e (博客更新)
 	base: "/AstroBlog/",
 	trailingSlash: "always",
 	integrations: [
